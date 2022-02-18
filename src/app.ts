5f41ab535686de28557ee2a7fce93f620f6cf100
Daniel <daniel.drozdzel@vattenfall.com>
2022-02-18 10:47:46 +0100

    add swagger

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import basicAuth from "express-basic-auth";
 import { Db } from "mongodb";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./config/swagger.json";
 
 import { errorLogger, logger } from "./logger";
 import { errorHandler, notFound } from "./errors";
@@ -10,18 +12,23 @@ const authPass: string = process.env.BASIC_AUTH as string;
 export const appFactory = (db: Db) => {
   const app = express();
 
+  app.use("/swagger-ui", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
   app.use(
     basicAuth({
       users: { task_creator: authPass },
     })
   );
 
-  app.use(express.json());
+  app.use(express.json());  
+
   app.use(logger);
 
   app.get("/", function (req, res, next) {
     res.send("Family-app is working");
   });
+
+
 
   app.use(errorLogger);
 
