f083b47d5059150ed4f0bd27765dbca9fff2b1bb
Daniel <daniel.drozdzel@vattenfall.com>
2022-02-27 21:17:35 +0100

    remove dist

--- dist/config/connection.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.connection = void 0;
-const mongodb_1 = require("mongodb");
-const url = process.env.MONGODB_URI;
-exports.connection = mongodb_1.MongoClient.connect(url).then(function (client) {
-    return client.db();
-});
--- dist/config/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.API_ROUTE = void 0;
-const API_ROUTE = "/api";
-exports.API_ROUTE = API_ROUTE;
