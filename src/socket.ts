d3e679ffcf450566852338f053a58d0978f0c47a
Daniel <daniel.drozdzel@vattenfall.com>
2022-08-09 08:00:00 +0200

    restore

--- src/socket.ts
+++ src/socket.ts
@@ -1,12 +1,11 @@
 import { Server } from "socket.io";
 import http from "http";
-import { connection } from "./config/connection";
 
 import { Db } from "mongodb";
 import chatRepositoryFactory from "./controllers/chat/chatRepository";
 
-export const socketFactory = (db: Db) => {
-  const server = http.createServer();
+export const socketFactory = (app: any, db: Db) => {
+  const server = http.createServer(app);
   const { add } = chatRepositoryFactory(db);
 
   const io = new Server(server, {
@@ -34,13 +33,3 @@ export const socketFactory = (db: Db) => {
 
   return server;
 };
-
-const chatPort: string = (process.env.CHAT_PORT as string) || "80";
-
-(async () => {
-  const chatServer = socketFactory(await connection);
-
-  chatServer.listen(chatPort, function () {
-    console.log(`Family app socket is listening on ${chatPort}`);
-  });
-})();
